080f64bedf3415f830f4d4e6b838f0883df383dc
xavierpr16 <xavycamacho@gmail.com>
2023-05-07 23:49:59 -0400

    Update 19.1

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+console. log("Your index.js file is loaded correctly!");
+
+$("") .on("click", function(){
+    $(".fadeMe").fadeToggle("slow");
+  });
+
+  let d = new Date();
+  alert("Today's date is " + d);
